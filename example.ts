57d8d0fc5e84a4672032084d8c5b8892a1f70b72
Matthew French <mfrench@mulethew.com>
2023-10-14 21:38:16 -0400

    split to class definition and example instansiation
    move instansiation code from repl.ts to example.ts
    add some notes and a license
    add replOptions interface and made parameters optional

---
+++ example.ts
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+import { simpleRepl } from './repl';
+import { request } from  'http';
+
+const commands: any = {
+  exit: { 
+    help: 'close the repl environment',
+    action: () => {
+      process.exit(0);
+    }
+  },
+  say: {
+    help: 'console.log the arguments to this command',
+    action: (args:Array<String>) => {
+      args.splice(0,1);
+      console.log(args.join(' '));
+    }
+  },
+  login: {
+    help: 'collect "login" credentials',
+    action: (args:Array<String>) => {
+      console.log(args[1]);
+      repl.interface.setPrompt('password: ');
+      repl.updateEval((password:string) => {
+        console.log('password = '+password)
+        repl.interface.setPrompt(repl.prompt);
+        repl.updateEval(repl.defaultEvaluate);
+      });
+      repl.interface.prompt();
+    }
+  },
+  request: { //this is "broken". simpleRepl does not wait till the action finishes before reprompting
+    help: 'make an http request `request <METHOD> <HOSTNAME> </PATH>`',
+    action: (args:Array<any>) => {
+      const options = {
+        hostname: args[2],
+        port: 80,
+        path: args[3],
+        method: args[1],
+        headers: {
+          accept: 'application/json'
+        }
+      };
+      
+      const req = request(options, (res) => {
+        console.log();
+        console.log(`STATUS: ${res.statusCode}`);
+        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
+        res.setEncoding('utf8');
+        res.on('data', (chunk) => {
+          console.log(`BODY: ${chunk}`);
+        });
+        res.on('end', () => {
+        });
+      });
+
+      req.on('error', (e) => {
+        console.error(`problem with request: ${e.message}`);
+      });
+      req.end();
+    }
+  }
+}
+
+const repl = new simpleRepl({prompt:'> ', commands: commands});
+/*
+interface replOptions {
+  prompt?: string;
+  input?: any;
+  output?: any;
+  evaluate?: Function;
+  commands?: any;        
+}
+*/
--- repl.ts
+++ repl.ts
@@ -1,26 +1,74 @@
-#!/usr/bin/env node
-
 import * as readline from 'readline';
-import { Stream } from 'stream';
-import { request } from  'http';
+
+interface replOptions {
+  prompt?: string;
+  input?: any; //TODO: fix input and output types. Should be ReadableStream/WritableStream?
+  output?: any;
+  evaluate?: Function;
+  commands?: any; //fix commands typing. should be object, maybe Object<Object>?
+}
 
 export class simpleRepl {
   prompt: string;
   inputStream: any; //TODO: fix input and output types. Should be ReadableStream/WritableStream?
   outputStream: any;
   evaluate: Function;
-  commands: Object;
+  commands: any; //fix commands typing. should be object, maybe Object<Object>?
   interface: readline.Interface;
+  defaultEvaluate: Function;
 
-  constructor(prompt:string, input:any, output:Stream, evaluate:Function, commands: Object){ //TODO: figure out issues with input type
-    this.prompt = prompt;
-    this.inputStream = input;
-    this.outputStream = output;  // TODO: defaults for these options? How do I make constructor accept undfined parameters? How do I make contructor accept an options json? 
-    this.evaluate = evaluate;
-    this.commands = commands;
+  constructor(options?: replOptions){ //TODO: figure out issues with input type
+
+    this.defaultEvaluate = (command:string) => {
+      const split = command.split(' ');
+
+      if(this.commands[split[0]]) {
+        if(split.length > 0){
+          this.commands[split[0]].action(split);
+        } else {
+          this.commands[split[0]].action();
+        }
+      } else {
+        console.log('Unrecognized command: '+split[0]);
+      }
+    }
+
+    const helpCommand: any = {
+      help: 'print help info for all defined commands.',
+      action: () => {
+        for (const command in this.commands) {
+          console.log(command + '\t\t' + this.commands[command].help);
+        }
+      }
+    }
+
+    const defaultCommands: any = {
+      exit: { 
+        help: 'close the repl environment',
+        action: () => {
+          process.exit(0);
+        }
+      },
+      say: {
+        help: 'console.log the arguments to this command',
+        action: (args:Array<String>) => {
+          args.splice(0,1);
+          console.log(args.join(' '));
+        }
+      },
+    }
+
+    options = typeof options !== 'undefined' ? options : {};
+
+    this.prompt = typeof options.prompt !== 'undefined' ? options.prompt : 'simpleRepl> ';
+    this.inputStream = typeof options.input !== 'undefined' ? options.input : process.stdin;
+    this.outputStream = typeof options.output !== 'undefined' ? options.output : process.stdout;
+    this.evaluate = typeof options.evaluate !== 'undefined' ? options.evaluate : this.defaultEvaluate;
+    this.commands = typeof options.commands !== 'undefined' ? options.commands : defaultCommands;
+    this.commands['help'] = helpCommand;
 
     this.interface = readline.createInterface(this.inputStream,this.outputStream)
-    this.interface.setPrompt(prompt);
+    this.interface.setPrompt(this.prompt);
     
     this.interface.prompt();
     this.interface.on('line',(line)=>{
@@ -44,94 +92,4 @@ export class simpleRepl {
   updateEval(newEval:any) { //TODO: fix newEval type? should be 'Function'?
     this.evaluate = newEval;
   }
-  
-
 }
-
-/* command definitions
-{
-  $name: {
-    help: '...',
-    action: () => {...}
-  }
-}
-*/
-
-let commands: any = {
-  exit: { 
-    help: 'close the repl environment',
-    action: () => {
-      process.exit(0);
-    }
-  },
-  say: {
-    help: 'console.log the arguments to this command',
-    action: (args:Array<String>) => {
-      args.splice(0,1);
-      console.log(args.join(' '));
-    }
-  },
-  login: {
-    help: 'collect "login" credentials',
-    action: (args:Array<String>) => {
-      console.log(args[1]);
-      repl.interface.setPrompt('password: ');
-      repl.updateEval((password:string) => {
-        console.log('password = '+password)
-        repl.interface.setPrompt(promptString);
-        repl.updateEval(defaultEvaluate);
-      });
-      repl.interface.prompt();
-    }
-  },
-  request: { //this is "broken". simpleRepl does not wait till the action finishes before reprompting
-    help: 'make an http request `request <METHOD> <HOSTNAME> </PATH>`',
-    action: (args:Array<any>) => {
-      const options = {
-        hostname: args[2],
-        port: 80,
-        path: args[3],
-        method: args[1],
-        headers: {
-          accept: 'application/json'
-        }
-      };
-      
-      const req = request(options, (res) => {
-        console.log(`STATUS: ${res.statusCode}`);
-        console.log(`HEADERS: ${JSON.stringify(res.headers)}`);
-        res.setEncoding('utf8');
-        res.on('data', (chunk) => {
-          console.log(`BODY: ${chunk}`);
-        });
-        res.on('end', () => {
-          console.log('No more data in response.');
-        });
-      });
-
-      req.on('error', (e) => {
-        console.error(`problem with request: ${e.message}`);
-      });
-      req.end();
-    }
-  }
-}
-
-
-const defaultEvaluate = (command:string) => {
-  const split = command.split(' ');
-
-  if(commands[split[0]]) {
-    if(split.length > 0){
-      commands[split[0]].action(split);
-    } else {
-      commands[split[0]].action();
-    }
-  } else {
-    console.log('Unrecognized command: '+split[0]);
-  }
-}
-
-const promptString = 'command> ' 
-
-const repl = new simpleRepl(promptString,process.stdin,process.stdout,defaultEvaluate,commands)
